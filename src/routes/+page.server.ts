d46e767f4cadbbfcc76ce0f258139273993a586c
Matus Cvengros <matus.cvengros@gmail.com>
2026-04-06 15:20:21 +0000

    feat: rename /status route to /dashboard

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	redirect(308, '/status');
+	redirect(308, '/dashboard');
 };
